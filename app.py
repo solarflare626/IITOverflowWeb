9fedf9ec0f1c8688a511e3079e103431c1089e23
Phillip Andrew Espina <Firiyuu@users.noreply.github.com>
2018-03-15 20:24:55 +0800

    Questions Made

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, request, render_template
-
+import requests
+from flask import Flask, render_template, request, flash,session,redirect,url_for
+from datetime import datetime
 
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -14,6 +14,15 @@ def index():
 def fillup():
 	return render_template('fillup.html')
 
+@app.route('/', methods=['GET','POST'])
+def question():
+    if request.method == 'POST':
+        date = str(datetime.now())
+        question_title = request.form['text']
+        question_desc = request.form['textarea']
+        response = requests.post('http://localhost:3000/api/Questions', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc,})
+        return redirect(url_for('question'))
+    return render_template('index.html')
 
-if __name__=='__main__':
-	app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=1)
