134235917a4ac28a141ff7613267428081aa1577
FrancoJigo <frankeejigzz@gmail.com>
2018-03-15 23:28:49 +0800

    made method to request for Categories from API

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template
-
-
+import json
+import requests
+import urllib
 
 app = Flask(__name__)
 
@@ -10,9 +11,18 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/fillup')
+@app.route('/fillupform', methods=['POST','GET'])
 def fillup():
-	return render_template('fillup.html')
+	url = ('http://localhost:3000/api/Categories?access_token=V4W9EdDy9iAACCotqJTot1XGyzxRYs4CSHCYlhVPJQHKoY1KpD2KUoWudDi5EgaH')
+	response = requests.get(url)
+	json_object = response.json()
+
+	categories = []
+
+	for i in range(len(json_object)):
+		categories.append(json_object[i]['name'])
+
+	return render_template('test.html', categories=categories)
 
 
 if __name__=='__main__':
