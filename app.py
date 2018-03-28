40827fc4c15d61b8aaecedcb6378cd639c9ac243
Phillip Andrew Espina <Firiyuu@users.noreply.github.com>
2018-03-28 13:47:26 +0800

    Final QandA Backend

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 import requests
 from flask import Flask, render_template, request, flash,session,redirect,url_for
 from datetime import datetime
+from urllib.parse import quote
+from urllib.request import urlopen
+import json
+from datetime import datetime
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-
-@app.route('/fillup')
-def fillup():
-	return render_template('fillup.html')
-
 @app.route('/', methods=['GET','POST'])
+def home(username=None):
+    return render_template("home.html")
+
+
+
+
+
+
+@app.route('/ask', methods=['GET','POST'])
 def question():
+
     if request.method == 'POST':
-        date = str(datetime.now())
         question_title = request.form['text']
         question_desc = request.form['textarea']
-        response = requests.post('http://localhost:3000/api/Questions', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc,})
+        requests.post('http://localhost:3000/api/Questions', json={"question": question_title, "questiondesc": question_desc,})
         return redirect(url_for('question'))
-    return render_template('index.html')
+
+
+
+
+    else:
+        
+        url = 'http://localhost:3000/api/Questions?filter[include]=answers'
+        html = urlopen(url).read().decode('utf-8')
+        questions = json.loads(html)
+
+
+        print(str(questions))
+
+        url1 = 'http://localhost:3000/api/Answers'
+        html1 = urlopen(url1).read().decode('utf-8')
+        answers = json.loads(html1)
+
+
+        print(str(questions))
+        return render_template('question.html', questions = questions, answers = answers)
 
 if __name__ == '__main__':
    app.run(debug=1)
