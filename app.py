038b8aa838d6943c108250be5ed73f7b171850f5
FrancoJigo <frankeejigzz@gmail.com>
2018-04-30 23:14:58 +0800

    added FollowedQuestions functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, render_template, url_for, redirect
 import requests
 from datetime import datetime
+from urllib.parse import quote
+from urllib.request import urlopen
 import json
 import os
 
@@ -10,12 +12,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/fillup')
 def fillup():
-	return render_template('fillup.html')
+    return render_template('fillup.html')
 
 
 @app.route('/ask', methods=['GET','POST'])
@@ -24,37 +26,82 @@ def question():
         date = str(datetime.now())
         question_title = request.form['title']
         question_desc = request.form['desc']
-        requests.post('http://iitoverflow.herokuapp.com/api/users/1/question', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc, "userId": 1})
+ # <<<<<<< HEAD
+        requests.post('http://iitoverflow.herokuapp.com/api/users/4/question', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc, "userId": 1})
+# =======
+#         requests.post('http://iitoverflow.herokuapp.com/api/users/1/question', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc, "userId": 1})
+# >>>>>>> 1a897cf8dbf28f4e6bb370dfc45f7dfd943ca04b
         return redirect(url_for('question'))
     return render_template('questions.html')
 
 
 
+# <<<<<<< HEAD
+# @app.route('/profile/',methods=['GET','POST'])
+# def profile():
+#     curuser = 4
+#     user =str(curuser)
+#     url = ('http://iitoverflow.herokuapp.com/api/Questions?filter[where][userId]=' + user)
+# =======
 @app.route('/profile/<int:id>',methods=['GET','POST'])
 def profile(id):
     user = str(id)
     url = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[include]=questions&filter[include]=interests')
     print(user)
+# >>>>>>> 1a897cf8dbf28f4e6bb370dfc45f7dfd943ca04b
     response = requests.get(url)
     json_object = response.json()
+    # html = urlopen(url).read().decode('utf-8')
+    # questions = json.loads(html)
 
-    curl = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[counts]=followers&filter[counts]=following&filter[include]=followers&filter[include]=following')
+# <<<<<<< HEAD
+#     # ParsedValue={}
+#     # ParsedValue1={}
+# =======
+#     curl = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[counts]=followers&filter[counts]=following&filter[include]=followers&filter[include]=following')
+#     response = requests.get(curl)
+#     json_object1 = response.json()
+#     val3 = json_object1['followersCount']
+#     val4 = json_object1['followingCount']
+
+#     url1 = ('http://http://iitoverflow.herokuapp.com/api/users/'+user+'/questionsfollowed?filter[include]=user')
+#     response = requests.get(url1)
+#     followed_questions = response.json()
+
+
+
+#     return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions = followed_questions)
+# # >>>>>>> 1a897cf8dbf28f4e6bb370dfc45f7dfd943ca04b
+
+    curl = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[counts]=followers&filter[counts]=following&filter[counts]=answers&filter[counts]=questionsfollowed&filter[include]=followers&filter[include]=following&filter[include]=answers&filter[include]=questionsfollowed')
     response = requests.get(curl)
     json_object1 = response.json()
     val3 = json_object1['followersCount']
     val4 = json_object1['followingCount']
+    val5 = json_object1['answersCount']
+    val6 = json_object1['questionsfollowedCount']
 
-    url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
-           '/questionsfollowed?filter={"include":{"relation":"user"}}')
-    response = requests.get(url)
-    followed_questions = response.json()
-    print(followed_questions)
+# <<<<<<< HEAD
+   
+    return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, answers=val5, questionsfollowed=val6)
+# =======
+#     url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
+#            '/questionsfollowed?filter={"include":{"relation":"user"}}')
+#     response = requests.get(url)
+#     followed_questions = response.json()
+#     print(followed_questions)
 
 
 
-    return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions)
+#     return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions)
+# >>>>>>> e178f465d1682c8c9973e22d3bdb4f027da8c73a
 
+    
 
 if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host= '0.0.0.0', port=port)
+# <<<<<<< HEAD
+    app.run(debug=True)  
+# =======
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(debug=True, host= '0.0.0.0', port=port)
+# # >>>>>>> 1a897cf8dbf28f4e6bb370dfc45f7dfd943ca04b
