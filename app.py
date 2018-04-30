91642e6fecbfdfeb455d32425b20c81c86c5d945
Schaveirios <eduardojr.quiobe@g.msuiit.edu.ph>
2018-04-30 17:27:51 +0800

    connecting ui to backend

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, url_for, redirect
 import requests
 from datetime import datetime
 import json
+import os
 
 
 app = Flask(__name__)
@@ -17,29 +18,36 @@ def fillup():
 	return render_template('fillup.html')
 
 
-
 @app.route('/ask', methods=['GET','POST'])
-def question():
+def question():	
     if request.method == 'POST':
         date = str(datetime.now())
         question_title = request.form['title']
         question_desc = request.form['desc']
-        requests.post('http://localhost:3000/api/users/1/question', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc, "userId": 1})
+        requests.post('http://iitoverflow.herokuapp.com/api/users/1/question', json={"question": question_title,"createdAt": date,  "updatedAt": date,"deletedAt": date,"questiondesc": question_desc, "userId": 1})
         return redirect(url_for('question'))
     return render_template('questions.html')
 
 
 
-@app.route('/profile/',methods=['GET','POST'])
-def profile():
-    curuser = 1
-    user =str(curuser)
-    url = ('http://localhost:3000/api/users/'+user+'?filter[include]=questions&filter[include]=interests')
+@app.route('/profile/<int:id>',methods=['GET','POST'])
+def profile(id):
+    user = str(id)
+    url = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[include]=questions&filter[include]=interests')
+    print(user)
     response = requests.get(url)
     json_object = response.json()
 
-    return render_template('profile.html', json_object=json_object)
+    curl = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[counts]=followers&filter[counts]=following&filter[include]=followers&filter[include]=following')
+    response = requests.get(curl)
+    json_object1 = response.json()
+    val3 = json_object1['followersCount']
+    val4 = json_object1['followingCount']
+
+
+    return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4)
 
 
 if __name__=='__main__':
-	app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host= '0.0.0.0', port=port)
