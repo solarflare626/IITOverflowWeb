473f174af6361e8e425c234e6bfd6117a5559193
joren35 <jorenpacaldo@yahoo.com>
2018-05-02 04:40:32 +0800

    Integrated View Specific question backend (not Final)

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,19 @@
 $('#autocomplete').autocomplete({
                 source: function (request, response) {
                     $.ajax({
-                      //  url: "http://127.0.0.1:3000/api/Tags?filter=%7B%20%22fields%22%3A%20%7B%22id%22%3A%20true%2C%20%22name%22%3A%20true%2C%20%22createdAt%22%3A%20false%2C%20%22updatedAt%22%3A%20false%2C%20%22deletedAt%22%3A%20false%2C%20%22categoryID%22%3A%20false%7D%7D",
-                        url: "http://127.0.0.1:3000/api/Tags?filter=%7B%20%22where%22%20%3A%7B%20%22name%22%20%3A%20%7B%20%22like%22%3A%20%22%25"+request.term+"%25%22%7D%7D%7D",
+                        url: "http://iitoverflow.herokuapp.com/api/Questions?filter[where][question][ilike]=%"+request.term+"%",
                         type: "GET",
                         dataType: "json",
-                        success: function (data) {;
-                            console.log(request);
-                            console.log(data);
+                        success: function (data) {
                             response($.map(data, function (item) {
-
                                 return {
-                                    label: item.name,
-                                    value: item.name
+                                    label: item.question,
+                                    value: item.id
                                 }
                             }));
                         }
                     });
-                },
+                }
 
-            })
+            });
 
