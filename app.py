c25402f5bf560c59e2193846049be99f5945c69f
FrancoJigo <frankeejigzz@gmail.com>
2018-05-12 20:44:04 +0800

    merge conflict in app.py file

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+from flask import Flask, jsonify, request, session, render_template
 import flask
-import sys
-import os
 import requests
-from datetime import datetime
-from urllib.parse import quote
 from urllib.request import urlopen
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify, json
+import json
+import os, sys
 
 app = Flask(__name__)
 app.secret_key = 'my very own secret key'
@@ -16,16 +14,14 @@ app.config['SESSION_TYPE'] = 'filesystem'
 def index():
     return render_template('landingpage2.html')
 
-
 @app.route('/categories', methods=['POST', 'GET'])
 def fillup():
     if request.method == 'POST':
         ids = request.json['ids']
         for i in ids:
             requests.post('http://iitoverflow.herokuapp.com/api/Interests',
-                          json={"categoryId": i, "userId": 1})
-
-        return redirect(url_for('newsfeed'))  # nextPage
+                          json={"categoryId": i, "userId": session['user']})
+        return jsonify({"message": "ok"})
 
     else:
         url = ('http://iitoverflow.herokuapp.com/api/Categories')
@@ -42,7 +38,7 @@ def login():
     u_id = params["userID"]
     session['token'] = u_token
     session['user'] = u_id
-    #return redirect(url_for('question'))
+    print(session['user'])
     return jsonify({"userID": session['user'], 'message': 'okay'})
 
 
@@ -52,44 +48,55 @@ def logout():
     session.pop('token', None)
     if 'user' in session:
         return jsonify({"message": "error"})
-
     return jsonify({"message": "okay"})
 
 
-@app.route('/newsfeed', methods=['GET', 'POST'])
+
+@app.route('/tags', methods=['GET','POST'])
+def tagslist():
+    urltag = 'http://iitoverflow.herokuapp.com/api/Tags'    
+    tagresponse = urlopen(urltag).read().decode('utf-8')
+    tagslist = json.loads(tagresponse)
+
+    alltags = []
+    for i in tagslist:
+        alltags.append(i['name'])
+
+
+@app.route('/newsfeed', methods=['GET','POST'])
 def question():
-    url = 'http://iitoverflow.herokuapp.com/api/Questions?filter={"include":[{"relation": "answers", "scope":{"include": {"relation": "user"}}}, {"relation":"category"}, {"relation": "tags"}]}'
+
+    url = 'http://iitoverflow.herokuapp.com/api/Questions?filter={"include":[{"relation":"answers","scope":{"include":[{"relation":"user"},{"relation":"comments"}]}},{"relation":"category"},{"relation":"tags"}]}'
 
     url2 = 'http://iitoverflow.herokuapp.com/api/Categories'
     response = requests.get(url2)
-    categories = response.json()
-
-    url3 = 'http://iitoverflow.herokuapp.com/api/Questions'
+    categories= response.json()
+    
     response1 = requests.get(url)
     questions = response1.json()
 
     # html = urlopen(url).read().decode('utf-8')
     # questions = json.loads(html)
-    url1 = 'http://iitoverflow.herokuapp.com/api/Answers'
+
+    url1 = 'http://iitoverflow.herokuapp.com/api/Answers?filter[include]=user'
     html1 = urlopen(url1).read().decode('utf-8')
     answers = json.loads(html1)
 
     url5 = 'http://iitoverflow.herokuapp.com/api/Tags'
     html5 = urlopen(url5).read().decode('utf-8')
     tag_list = json.loads(html5)
-
+    
     newlist = []
     for i in tag_list:
         newlist.append(i['name'])
 
-    return render_template('question2.html', tag_list=newlist, questions=questions, answers=answers, categories=categories)
+    return render_template('question2.html', tag_list = newlist, questions = questions, categories=categories, answers=answers)
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile2():
     user = str(session['user'])
     url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
            '?filter[include]=questions&filter[include]=interests')
-    print(user)
     response = requests.get(url)
     json_object = response.json()
 
@@ -111,95 +118,42 @@ def profile2():
            '/answers?filter[include]=user&filter[include]=question')
     response = requests.get(url)
     answered_questions = response.json()
-    
+
     return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions, answers=val5, questionsfollowed=val6, answered_questions=answered_questions)
 
-@app.route('/profile/<int:id>',methods=['GET','POST'])
+@app.route('/profile/<int:id>', methods=['GET', 'POST'])
 def profile(id):
     user = str(id)
-    curr = 1
-    curuser = str(curr)
-    print(curr)
-    if id == curr:
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[include]=questions&filter[include]=interests')
-        print(user)
-        response = requests.get(url)
-        json_object = response.json()
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user +'/questionsfollowed?filter={"include":{"relation":"user"}}')
-        response = requests.get(url)
-        followed_questions = response.json()
+    url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
+           '?filter[include]=questions&filter[include]=interests')
+    response = requests.get(url)
+    json_object = response.json()
 
-        curl = ('http://iitoverflow.herokuapp.com/api/users/'+user +'?filter[counts]=followers&filter[counts]=following&filter[counts]=answers&filter[counts]=questionsfollowed&filter[counts]=questions&filter[include]=followers&filter[include]=following&filter[include]=answers&filter[include]=questionsfollowed&filter[include]=questions')
-        response = requests.get(curl)
-        json_object1 = response.json()
-        val3 = json_object1['followersCount']
-        val4 = json_object1['followingCount']
-        val5 = json_object1['answersCount']
-        val6 = json_object1['questionsfollowedCount']
-        val7 = json_object1['questionsCount']
+    url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
+           '/questionsfollowed?filter={"include":{"relation":"user"}}')
+    response = requests.get(url)
+    followed_questions = response.json()
 
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user +'/answers?filter[include]=user&filter[include]=question')
-        response = requests.get(url)
-        answered_questions = response.json()
+    curl = ('http://iitoverflow.herokuapp.com/api/users/'+user +
+            '?filter[counts]=followers&filter[counts]=following&filter[counts]=answers&filter[counts]=questionsfollowed&filter[include]=followers&filter[include]=following&filter[include]=answers&filter[include]=questionsfollowed')
+    response = requests.get(curl)
+    json_object1 = response.json()
+    val3 = json_object1['followersCount']
+    val4 = json_object1['followingCount']
+    val5 = json_object1['answersCount']
+    val6 = json_object1['questionsfollowedCount']
 
-        return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions, answers=val5, questionsfollowed=val6, questions=val7, nswered_questions=answered_questions)
-    
-    else:
-        followurl = ('http://iitoverflow.herokuapp.com/api/users/'+curuser+ '/following/rel/'+user+'')
-        response = requests.head(followurl)
-        print(response.status_code)
-        s_code = response.status_code
+    url = ('http://iitoverflow.herokuapp.com/api/users/'+user +
+           '/answers?filter[include]=user&filter[include]=question')
+    response = requests.get(url)
+    answered_questions = response.json()
 
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user+'?filter[include]=questions&filter[include]=interests')
-        print(user)
-        response = requests.get(url)
-        json_object = response.json()
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user +'/questionsfollowed?filter={"include":{"relation":"user"}}')
-        response = requests.get(url)
-        followed_questions = response.json()
+    return render_template('profile.html', json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions, answers=val5, questionsfollowed=val6, answered_questions=answered_questions)
 
-        curl = ('http://iitoverflow.herokuapp.com/api/users/'+user +'?filter[counts]=followers&filter[counts]=following&filter[counts]=answers&filter[counts]=questionsfollowed&filter[counts]=questions&filter[include]=followers&filter[include]=following&filter[include]=answers&filter[include]=questionsfollowed&filter[include]=questions')
-        response = requests.get(curl)
-        json_object1 = response.json()
-        val3 = json_object1['followersCount']
-        val4 = json_object1['followingCount']
-        val5 = json_object1['answersCount']
-        val6 = json_object1['questionsfollowedCount']
-        val7 = json_object1['questionsCount']
-
-        url = ('http://iitoverflow.herokuapp.com/api/users/'+user +'/answers?filter[include]=user&filter[include]=question')
-        response = requests.get(url)
-        answered_questions = response.json()
-        
-        return render_template('profileforOtherUser.html', curr= curr, curuser= curuser, id=id, s_code= s_code, json_object=json_object, json_object1=json_object1, followers=val3, following=val4, followed_questions=followed_questions, answers=val5, questionsfollowed=val6, questions=val7, nswered_questions=answered_questions)
-
-@app.route('/tags', methods=['GET','POST'])
-def tagslist():
-   
-    urlCategory = 'http://iitoverflow.herokuapp.com/api/Categories?filter[include]=tags'   
-    
-    catresponse = urlopen(urlCategory).read().decode('utf-8')
-    categorylist = json.loads(catresponse)
-
-
-    return render_template('tags.html', category=categorylist)
 
 @app.route('/getCurrentUser', methods=['GET'])
 def getSession():
     return jsonify({"message": str(session['user'])})
-
-@app.route('/tagsQ/<int:id>', methods=['GET','POST'])
-def tagsQ(id):
-   
-    urlCategory = 'http://iitoverflow.herokuapp.com/api/Tags/' +str(id)+'?filter={"include":[{"relation":"questions","scope":{"include":[{"relation":"user"},{"relation":"category"},{"relation":"upvotes"},{"relation":"answers","scope":{"include":"comments"}}]}}]}'
-    
-    catresponse = urlopen(urlCategory).read().decode('utf-8')
-    categorylist = json.loads(catresponse)
-
-
-    return render_template('specifictags.html', questions= categorylist['questions'])
-
-    
 
 @app.after_request
 def add_cors(resp):
@@ -209,12 +163,14 @@ def add_cors(resp):
     resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET, PUT, DELETE'
     resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get('Access-Control-Request-Headers',
                                                                              'Authorization')
+                                                                             
     # set low for debugging
 
     if app.debug:
         resp.headers["Access-Control-Max-Age"] = '1'
     return resp
 
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, use_reloader=True, host='0.0.0.0', port=port)
+    app.run(host='localhost', debug=True)
+
