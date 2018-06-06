6782c405b4f57dc3d14b50aeba29a174951a3deb
FrancoJigo <frankeejigzz@gmail.com>
2018-06-06 14:40:06 +0800

    Fixed chat-logout and edit-profile errors

--- static/js/app2.js
+++ static/js/app2.js
@@ -54,7 +54,10 @@ $(document).on('click', '.follow2', function() {
     $(this).removeClass('wait');
 });
 
+
+
 $(document).on('click', '#signout', function() {
+    console.log("logout");
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.disconnect();
     auth2.signOut().then(function() {
